c600d3409b6e8846f243ed7f33bcc88677067ba9
6h15m <mkitwave@gmail.com>
2023-11-05 02:06:16 +0900

    모바일 스타일 조정

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <title>{BLOG_TITLE}</title>
       </Head>
       <div className="flex flex-col h-full w-full">
-        <h1 className="shrink-0 md:text-[20rem] text-[12rem] md:break-words text-gray-900 flex pb-2 tracking-tight leading-tight md:pr-8">
-          Zero. log
+        <h1 className="shrink-0 lg:text-start text-center lg:pt-0 pt-10 lg:text-[20rem] text-[7rem] w-full text-gray-900 pb-2 tracking-tight leading-tight md:pr-8">
+          Zero.log
         </h1>
         <KeyVisual />
       </div>
