79b6a44f636868153c0e9ec389e39cdf9cb50b03
Yeseo Lee <mkitwave@gmail.com>
2023-11-13 14:47:48 +0900

    style: Header Padding 변경 (#10)

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -36,7 +36,7 @@ export default function Posts({ allPosts }: Props) {
         <title>Posts | {BLOG_TITLE}</title>
       </Head>
       <div className="flex flex-col h-full w-full py-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 w-full pb-10">
           <div className="lg:h-[20rem] h-[5rem] rounded text-xl">
             {tags.map((tag) => (
               <button
