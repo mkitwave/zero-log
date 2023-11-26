d929515543509f6e7052c09ebb4029aa935486cb
Yeseo Lee <mkitwave@gmail.com>
2023-11-27 03:37:55 +0900

    style: Header style 변경 (#15)

--- components/Layout/header.tsx
+++ components/Layout/header.tsx
@@ -9,44 +9,46 @@ export const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="flex border-b gap-x-4 justify-between border-gray-500 items-center h-20">
-      <div className="md:gap-x-4 gap-x-2 flex text-lg md:text-2xl items-center font-light">
-        {pathname !== "/" && (
-          <Link href="/" className="mr-2">
-            <Image
-              src={keyVisual}
-              alt="key visual"
-              width={50}
-              className="md:w-10 md:h-10 h-8 w-8"
-            />
+    <header className="fixed md:px-6 md:pt-2 px-4 w-full bg-gray-50">
+      <div className="w-full flex border-b gap-x-4 justify-between border-gray-500 items-center md:h-20 h-16">
+        <div className="md:gap-x-4 gap-x-2 flex text-lg md:text-xl items-center font-light">
+          {pathname !== "/" && (
+            <Link href="/" className="mr-2">
+              <Image
+                src={keyVisual}
+                alt="key visual"
+                width={50}
+                className="md:w-10 md:h-10 h-8 w-8"
+              />
+            </Link>
+          )}
+          <Link href="/posts" className="hover:decoration-1 hover:underline">
+            Posts
           </Link>
-        )}
-        <Link href="/posts" className="hover:decoration-1 hover:underline">
-          Posts
-        </Link>
-        <Link href="/resume" className="hover:decoration-1 hover:underline">
-          Resume
-        </Link>
-      </div>
-      <div className="gap-x-3 md:text-md text-sm flex text-black">
-        <a
-          href={PERSONAL_LINKS.twitter}
-          target="_blank"
-          className="rounded-full md:border md:border-gray-500 h-10 flex gap-x-1.5 items-center md:px-3 justify-center"
-        >
-          <TfiTwitterAlt className="md:hidden w-6 h-6" />
-          <span className="md:flex hidden">Twitter</span>
-          <TfiArrowTopRight className="w-4 h-4 mt-1 md:flex hidden" />
-        </a>
-        <a
-          href={PERSONAL_LINKS.github}
-          target="_blank"
-          className="rounded-full md:border md:border-gray-500 h-10 flex gap-x-1.5 items-center md:px-3 justify-center"
-        >
-          <TfiGithub className="md:hidden w-6 h-6" />
-          <span className="md:flex hidden">Github</span>
-          <TfiArrowTopRight className="w-4 h-4 mt-1 md:flex hidden" />
-        </a>
+          <Link href="/resume" className="hover:decoration-1 hover:underline">
+            Resume
+          </Link>
+        </div>
+        <div className="gap-x-3 md:text-md text-sm flex text-black">
+          <a
+            href={PERSONAL_LINKS.twitter}
+            target="_blank"
+            className="rounded-full md:border md:border-gray-500 h-10 flex gap-x-1.5 items-center md:px-3 justify-center"
+          >
+            <TfiTwitterAlt className="md:hidden w-6 h-6" />
+            <span className="md:flex hidden">Twitter</span>
+            <TfiArrowTopRight className="w-4 h-4 mt-1 md:flex hidden" />
+          </a>
+          <a
+            href={PERSONAL_LINKS.github}
+            target="_blank"
+            className="rounded-full md:border md:border-gray-500 h-10 flex gap-x-1.5 items-center md:px-3 justify-center"
+          >
+            <TfiGithub className="md:hidden w-6 h-6" />
+            <span className="md:flex hidden">Github</span>
+            <TfiArrowTopRight className="w-4 h-4 mt-1 md:flex hidden" />
+          </a>
+        </div>
       </div>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <Head>
         <title>{BLOG_TITLE}</title>
       </Head>
-      <div className="flex flex-col h-full w-full">
-        <h1 className="shrink-0 lg:text-start text-center lg:pt-0 pt-10 lg:text-[20rem] text-[7rem] w-full text-gray-900 pb-2 tracking-tight leading-tight md:pr-8">
+      <div className="flex flex-col h-full lg:overflow-hidden w-full">
+        <h1 className="shrink-0 lg:text-start text-center lg:pt-0 pt-10 lg:text-[16rem] text-[6rem] w-full text-gray-900 pb-2 tracking-tight leading-tight md:pr-8">
           Zero.log
         </h1>
         <KeyVisual />
