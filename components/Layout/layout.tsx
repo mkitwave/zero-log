25ccdb7cbc29a55a2be4504b13cfcbf6c9ab3e57
6h15m <mkitwave@gmail.com>
2023-11-27 03:35:56 +0900

    style: Header style 변경

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -12,10 +12,11 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Meta />
-      <div className="h-screen w-full">
-        <div className="md:px-6 md:py-2 p-4 flex flex-col h-full w-full">
-          <Header />
-          <div className="h-0 grow w-full">{children}</div>
+      <div className="h-screen w-full flex flex-col">
+        <Header />
+        <div className="md:h-20 h-16" />
+        <div className="md:px-6 md:py-2 p-4 flex flex-col w-full h-0 grow">
+          {children}
         </div>
       </div>
     </>
