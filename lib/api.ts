35e117e69ec0ff8b01b2b48093069d9e4afe7ec8
zero <mkitwave@gmail.com>
2023-12-02 10:36:39 +0900

    fix: shiki 동작 수정

--- lib/api.ts
+++ lib/api.ts
@@ -3,17 +3,40 @@ import { join } from "path";
 import PostType from "../interfaces/post";
 import { serialize } from "next-mdx-remote/serialize";
 import remarkGfm from "remark-gfm";
-import rehypePrettyCode from "rehype-pretty-code";
-import { getHighlighter } from "shiki";
+import rehypeCode, { Options as RehypeCodeOptions } from "rehype-pretty-code";
+import { noop } from "@fxts/core";
+import * as shiki from "shiki";
 
 const postsDirectory = join(process.cwd(), "_posts");
-const themePath = join(process.cwd(), "lib/shiki/theme/solarized-light.json");
+const shikiDirectory = join(process.cwd(), "lib/shiki");
 
 export function getPostSlugs() {
   return fs
     .readdirSync(postsDirectory)
     .map((slug) => slug.replace(/\.mdx$/, ""));
 }
+
+const touched = { current: false };
+
+const touchShikiPath = (): void => {
+  if (touched.current) return;
+  fs.readdir(shikiDirectory, noop);
+  touched.current = true;
+};
+
+const getHighlighter: RehypeCodeOptions["getHighlighter"] = async (options) => {
+  touchShikiPath();
+
+  const highlighter = await shiki.getHighlighter({
+    ...(options as any),
+    paths: {
+      languages: `${shikiDirectory}/languages/`,
+      themes: `${shikiDirectory}/themes/`,
+    },
+  });
+
+  return highlighter;
+};
 
 export const getPostSourceBySlug = async (slug: string) => {
   const fullPath = join(postsDirectory, `${slug}.mdx`);
@@ -26,9 +49,10 @@ export const getPostSourceBySlug = async (slug: string) => {
       rehypePlugins: [
         [
           // @ts-ignore @TODO rehype-pretty-code 의존성 오류
-          rehypePrettyCode,
+          rehypeCode,
           {
             theme: "solarized-light",
+            getHighlighter,
             keepBackground: false,
           },
         ],
