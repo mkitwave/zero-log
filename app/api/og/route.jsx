228020993744dfd04cd1cdd6dc4512996b34fc41
zero <mkitwave@gmail.com>
2024-03-02 20:49:12 +0900

    fix: 용량 개선

--- app/api/og/route.jsx
+++ app/api/og/route.jsx
@@ -4,23 +4,47 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const title = searchParams.get("title");
   const excerpt = searchParams.get("excerpt");
-  const fontData = await fetch(
-    new URL("public/assets/font/GmarketSansTTFMedium.ttf", import.meta.url),
-  ).then((res) => res.arrayBuffer());
   return new ImageResponse(
     (
       <div
-        tw="h-full w-full flex p-10 flex-col items-end text-black"
         style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          padding: "2.5rem",
+          flexDirection: "column",
+          alignItems: "flex-end",
+          color: "black",
           backgroundColor: "#F9FAFB",
           wordBreak: "break-all",
         }}
       >
-        <div tw="w-full h-0 grow flex flex-col justify-center">
-          <span tw="text-4xl leading-loose pb-8">{title}</span>
-          {excerpt && <span tw="opacity-60 text-lg">{excerpt}</span>}
+        <div
+          style={{
+            width: "100%",
+            height: "0",
+            display: "flex",
+            flexGrow: "1",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <span
+            style={{
+              fontSize: "2.25rem",
+              lineHeight: "2",
+              paddingBottom: "2rem",
+            }}
+          >
+            {title}
+          </span>
+          {excerpt && (
+            <span style={{ opacity: "60%", fontSize: "1.125rem" }}>
+              {excerpt}
+            </span>
+          )}
         </div>
-        <span tw="text-2xl">Zero.log</span>
+        <span style={{ fontSize: "1.5rem" }}>Zero.log</span>
       </div>
     ),
     {
