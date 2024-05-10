0eb71a4103fcd02b0820c8734e368a7ddfb4bfed
zero <mkitwave@gmail.com>
2024-05-10 11:11:36 +0900

    test: orientation 동작 테스트

--- components/Home/Eyes.tsx
+++ components/Home/Eyes.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { checkIsMobile } from "../../lib/isMobile";
 import { Eye } from "./Eye";
 
@@ -12,42 +12,6 @@ interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
 export const Eyes = () => {
   const [orientation, setOrientation] = useState<string>("undefined");
 
-  useEffect(() => {
-    if (!checkIsMobile()) {
-      alert("모바일 아님");
-
-      return;
-    }
-
-    const deviceOrientationEvent =
-      window.DeviceOrientationEvent as unknown as DeviceOrientationEventiOS;
-
-    if (
-      deviceOrientationEvent !== undefined &&
-      typeof deviceOrientationEvent.requestPermission === "function"
-    ) {
-      deviceOrientationEvent.requestPermission().then((state) => {
-        alert(state);
-        if (state === "granted") {
-          alert("granted");
-          window.addEventListener("deviceorientation", (event) => {
-            setOrientation(
-              `${event.absolute}, ${event.alpha}, ${event.beta}, ${event.gamma}`,
-            );
-          });
-        } else if (state === "denied") {
-          alert("denied");
-        }
-      });
-    } else {
-      window.addEventListener("deviceorientation", (event) => {
-        setOrientation(
-          `${event.absolute}, ${event.alpha}, ${event.beta}, ${event.gamma}`,
-        );
-      });
-    }
-  }, []);
-
   return (
     <>
       <Canvas shadows camera={{ position: [0, 0, 24], fov: 40 }}>
@@ -57,6 +21,46 @@ export const Eyes = () => {
         <Eye position={[4.5, 0, 1]} />
       </Canvas>
       <span className="absolute text-black">{orientation}</span>
+      <button
+        className="absolute"
+        onClick={() => {
+          if (!checkIsMobile()) {
+            alert("모바일 아님");
+
+            return;
+          }
+
+          const deviceOrientationEvent =
+            window.DeviceOrientationEvent as unknown as DeviceOrientationEventiOS;
+
+          if (
+            deviceOrientationEvent !== undefined &&
+            typeof deviceOrientationEvent.requestPermission === "function"
+          ) {
+            deviceOrientationEvent.requestPermission().then((state) => {
+              alert(state);
+              if (state === "granted") {
+                alert("granted");
+                window.addEventListener("deviceorientation", (event) => {
+                  setOrientation(
+                    `${event.absolute}, ${event.alpha}, ${event.beta}, ${event.gamma}`,
+                  );
+                });
+              } else if (state === "denied") {
+                alert("denied");
+              }
+            });
+          } else {
+            window.addEventListener("deviceorientation", (event) => {
+              setOrientation(
+                `${event.absolute}, ${event.alpha}, ${event.beta}, ${event.gamma}`,
+              );
+            });
+          }
+        }}
+      >
+        Grant Access
+      </button>
     </>
   );
 };
